fe4652268d5865174b176416785d885f42b51756
= <augustodemeloh@protonmail.com>
2024-04-09 21:30:46 -0300

    feat: not null and auto-increment constraints
    all ids are now auto-increment columns and all columns are not null.

--- database/design/ddl.sql
+++ database/design/ddl.sql
@@ -1,42 +1,44 @@
-CREATE DATABASE 095cm;
+CREATE DATABASE conexao095;
 
 -- Tables --
 CREATE TABLE IF NOT EXISTS users (
-	id INT,
-    is_admin BOOLEAN,
-    selected_avatar INT <<FK>>
-    nickname VARCHAR(20),
-    password TEXT,
-    xp INT
+	id INT NOT NULL,
+    is_admin BOOLEAN NOT NULL,
+    selected_avatar INT NOT NULL,
+    nickname VARCHAR(20) NOT NULL,
+    password TEXT NOT NULL,
+    xp INT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS photos (
-	id INT,
-    user_id INT,
-    filename TEXT,
-    latitude FLOAT,
-    longitude FLOAT
+	id INT NOT NULL,
+    user_id INT NOT NULL,
+    filename TEXT NOT NULL,
+    latitude FLOAT NOT NULL,
+    longitude FLOAT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS avatars (
-	id INT,
-    name VARCHAR(30),
-    filename TEXT,
+	id INT NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    filename TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS user_avatars (
-	user_id INT,
-    avatar_id INT
+	user_id INT NOT NULL,
+    avatar_id INT NOT NULL
 );
 
 -- Constraints --
 ALTER TABLE users
+MODIFY id INT NOT NULL AUTO_INCREMENT,
 ADD CONSTRAINT pk_users_id PRIMARY KEY (id),
 ADD CONSTRAINT uk_users_nickname UNIQUE KEY (nickname),
 ADD CONSTRAINT fk_users_selected_avatar FOREIGN KEY (selected_avatar) REFERENCES avatars(id) ON UPDATE CASCADE ON DELETE RESTRICT,
 ADD INDEX idx_users_nickname (nickname);
 
 ALTER TABLE photos
+MODIFY id INT NOT NULL AUTO_INCREMENT,
 ADD CONSTRAINT pk_photos_id PRIMARY KEY (id),
 ADD CONSTRAINT fk_photos_user_id FOREIGN KEY (user_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE,
 ADD INDEX idx_photos_user_id (user_id),
@@ -44,6 +46,7 @@ ADD INDEX idx_photos_latitude (latitude),
 ADD INDEX idx_photos_longitude (longitude);
 
 ALTER TABLE avatars
+MODIFY id INT NOT NULL AUTO_INCREMENT,
 ADD CONSTRAINT pk_avatars_id PRIMARY KEY (id),
 ADD CONSTRAINT uk_avatars_name UNIQUE KEY (name),
 ADD INDEX idx_avatars_name (name);
