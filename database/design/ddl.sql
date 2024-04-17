3573f6eb84f23151575225f6c710ce20fb53771e
= <augustodemeloh@protonmail.com>
2024-04-17 13:44:38 -0300

    refactor: some pk/fk to unsigned bigint
    The max number of a signed int is 2147483647, and for an unsigned bigint is 8.5x bigger. This is useful for tables like users and photos.

--- database/design/ddl.sql
+++ database/design/ddl.sql
@@ -1,50 +1,49 @@
 CREATE DATABASE conexao095;
-
 -- Tables --
 CREATE TABLE IF NOT EXISTS users (
-	id INT NOT NULL,
+	id BIGINT UNSIGNED NOT NULL,
     is_admin BOOLEAN NOT NULL,
-    selected_avatar INT NOT NULL,
+    selected_avatar INT UNSIGNED NOT NULL,
     nickname VARCHAR(20) NOT NULL,
     password TEXT NOT NULL,
-    xp INT NOT NULL
+    xp INT UNSIGNED NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS photos (
-	id INT NOT NULL,
-    user_id INT NOT NULL,
+	id BIGINT UNSIGNED NOT NULL,
+    user_id BIGINT UNSIGNED NOT NULL,
     filename TEXT NOT NULL,
     latitude FLOAT NOT NULL,
     longitude FLOAT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS avatars (
-	id INT NOT NULL,
+	id INT UNSIGNED NOT NULL,
     name VARCHAR(30) NOT NULL,
     filename TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS user_avatars (
-	user_id INT NOT NULL,
-    avatar_id INT NOT NULL
+	user_id BIGINT UNSIGNED NOT NULL,
+    avatar_id INT UNSIGNED NOT NULL
 );
 
 -- Constraints --
 ALTER TABLE avatars
-MODIFY id INT NOT NULL AUTO_INCREMENT,
+MODIFY id INT UNSIGNED NOT NULL AUTO_INCREMENT,
 ADD CONSTRAINT pk_avatars_id PRIMARY KEY (id),
 ADD CONSTRAINT uk_avatars_name UNIQUE KEY (name),
 ADD INDEX idx_avatars_name (name);
 
 ALTER TABLE users
-MODIFY id INT NOT NULL AUTO_INCREMENT,
+MODIFY id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
 ADD CONSTRAINT pk_users_id PRIMARY KEY (id),
 ADD CONSTRAINT uk_users_nickname UNIQUE KEY (nickname),
 ADD CONSTRAINT fk_users_selected_avatar FOREIGN KEY (selected_avatar) REFERENCES avatars(id) ON UPDATE CASCADE ON DELETE RESTRICT,
 ADD INDEX idx_users_nickname (nickname);
 
 ALTER TABLE photos
-MODIFY id INT NOT NULL AUTO_INCREMENT,
+MODIFY id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
 ADD CONSTRAINT pk_photos_id PRIMARY KEY (id),
 ADD CONSTRAINT fk_photos_user_id FOREIGN KEY (user_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE,
 ADD INDEX idx_photos_user_id (user_id),
