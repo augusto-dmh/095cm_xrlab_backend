ed1ccef1da5f7a4bafdc7c7810838352aea42f3e
= <augustodemeloh@protonmail.com>
2024-05-17 22:12:34 -0300

    feat: drop database choice in script
    if the dev database already exists, then it'll be prompted an option between dropping the database and continue or not, exiting the script.

--- api/src/database/scripts/create_db_dev.sh
+++ api/src/database/scripts/create_db_dev.sh
@@ -10,7 +10,6 @@ while true; do
     echo -e "\nPlease enter your MySQL password:"
     read -s password
 
-    # Try to connect to MySQL without doing anything
     if mysql -h $hostname -u $username -p$password -e "quit" 2>/dev/null; then
         echo -e "\nSuccessfully connected to MySQL."
         break
@@ -19,15 +18,31 @@ while true; do
     fi
 done
 
-# Try to create the database
+if mysql -h $hostname -u $username -p$password -e "USE conexao095_DEV" 2>/dev/null; then
+    echo -e "\nDatabase 'conexao095_DEV' already exists."
+    read -p "Do you want to drop the existing database? (yes/no): " drop_choice
+    if [[ "$drop_choice" == "yes" ]]; then
+        mysql -h $hostname -u $username -p$password -e "DROP DATABASE conexao095_DEV"
+        if [ $? -eq 0 ]; then
+            echo -e "\nSuccessfully dropped existing database 'conexao095_DEV'."
+        else
+            echo -e "\nFailed to drop database. Exiting the script."
+            exit 1
+        fi
+    else
+        echo -e "\nExiting the script."
+        exit 0
+    fi
+fi
+
 mysql -h $hostname -u $username -p$password <<EOF
 CREATE DATABASE conexao095_DEV;
 EOF
 
 if [ $? -eq 0 ]; then
-    echo -e "\nSuccessfully created database 'conexao095'."
+    echo -e "\nSuccessfully created database 'conexao095_DEV'."
 else
-    echo "Failed to create database 'conexao095'. Exiting the script."
+    echo -e "\nFailed to create database 'conexao095_DEV'. Exiting the script."
     exit 1
 fi
 
@@ -38,5 +53,4 @@ cd ../../../
 npx sequelize-cli db:migrate
 
 echo "Inserting mock data..."
-
 npx sequelize-cli db:seed:all
