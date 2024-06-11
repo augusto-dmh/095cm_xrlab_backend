c5f1a285acadb77a8cd67cdf06f0db61e42ba36f
= <augustodemeloh@protonmail.com>
2024-06-11 11:13:01 -0300

    refactor: user field validators separated in module
    the user fields validators are now in a separated module: one that exports two validators for validating on storing or updating a user.

--- src/routes/user.js
+++ src/routes/user.js
@@ -6,122 +6,16 @@ import * as validations from "../validation";
 import * as errors from "../validation/errors/models";
 import Avatar from "../models/Avatar";
 import User from "../models/User";
+import userValidator from "../middlewares/validators/user";
 
 const router = new Router();
 
-const fieldsValidation = [
-  [
-    body("nickname")
-      .exists()
-      .withMessage(errors.nickname.empty)
-      .bail()
-      .isLength({ min: 0, max: 20 })
-      .withMessage(errors.nickname.invalidLength)
-      .escape(),
-  ],
-  [
-    body("birthdate")
-      .exists()
-      .withMessage(errors.birthdate.empty)
-      .bail()
-      .isISO8601("yyyy-mm-dd")
-      .withMessage(errors.birthdate.nonDate)
-      .escape(),
-  ],
-  [
-    body("isAdmin")
-      .optional()
-      .isBoolean()
-      .withMessage(errors.isAdmin.nonBoolean)
-      .bail(),
-  ],
-  [
-    body("selectedAvatar")
-      .optional()
-      .exists()
-      .withMessage(errors.selectedAvatar.empty)
-      .bail(),
-  ],
-  [
-    body("password")
-      .exists()
-      .withMessage(errors.password.empty)
-      .bail()
-      .isLength({ min: 6, max: 50 })
-      .withMessage(errors.password.invalidLength),
-  ],
-  [
-    body("xp")
-      .optional()
-      .exists()
-      .withMessage(errors.xp.empty)
-      .bail()
-      .isInt()
-      .withMessage(errors.xp.nonInteger),
-  ],
-];
-
-const fieldsValidationUpdate = [
-  [
-    body("nickname")
-      .optional()
-      .exists()
-      .withMessage(errors.nickname.empty)
-      .bail()
-      .isLength({ min: 0, max: 20 })
-      .withMessage(errors.nickname.invalidLength)
-      .escape(),
-  ],
-  [
-    body("birthdate")
-      .optional()
-      .exists()
-      .withMessage(errors.birthdate.empty)
-      .bail()
-      .isISO8601("yyyy-mm-dd")
-      .withMessage(errors.birthdate.nonDate)
-      .escape(),
-  ],
-  [
-    body("isAdmin")
-      .optional()
-      .isBoolean()
-      .withMessage(errors.isAdmin.nonBoolean)
-      .bail(),
-  ],
-  [
-    body("selectedAvatar")
-      .optional()
-      .exists()
-      .withMessage(errors.selectedAvatar.empty)
-      .bail(),
-  ],
-  [
-    body("password")
-      .optional()
-      .exists()
-      .withMessage(errors.password.empty)
-      .bail()
-      .isLength({ min: 6, max: 50 })
-      .withMessage(errors.password.invalidLength),
-  ],
-  [
-    body("xp")
-      .optional()
-      .exists()
-      .withMessage(errors.xp.empty)
-      .bail()
-      .isInt()
-      .withMessage(errors.xp.nonInteger),
-  ],
-];
-
-router.post("/users", fieldsValidation, userController.store);
+router.post("/users", userValidator.store(), userController.store);
 router.get("/users", userController.index);
 router.get("/users/user", loginRequired, userController.show);
 router.put(
   "/users",
-  fieldsValidationUpdate,
+  userValidator.update(),
   loginRequired,
   userController.update
 );
